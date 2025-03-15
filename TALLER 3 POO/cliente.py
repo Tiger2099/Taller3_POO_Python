07058abac7288781f69fc9b5876f4d03b747c1bd
Tiger2099 <joseemmanuelparravillamizar@gmail.com>
2025-03-15 02:56:42 +0100

    V0.4
    Agregamos mas cosas XD

--- TALLER 3 POO/cliente.py
+++ TALLER 3 POO/cliente.py
@@ -4,46 +4,41 @@ class cliente:
     #se crea el modelo constructor
     def __init__(self):
         #se crean atributos de clase
-        self.nombre_cliente = ""
-        self.apellido_cliente = ""
+        self.nombre_cliente = " "
+        self.apellido_cliente = " "
+
+
+
    #crear metodos get y set por atributos
-def get_nombre(self):
-    return self.nombre_cliente
+    def get_nombre(self):
+        return self.nombre_cliente
 
-def get_apellido(self):
-    return self.apellido_cliente
+    def get_apellido(self):
+        return self.apellido_cliente
 
-def set_nombre(self, dato_nombre):
-   self.nombre_cliente = dato_nombre
+    def set_nombre(self, dato_nombre):
+        self.nombre_cliente = dato_nombre
 
-def set_apellido(self, dato_apellido):
-   self.apellido_cliente = dato_apellido
+    def set_apellido(self, dato_apellido):
+        self.apellido_cliente = dato_apellido
+
    
    
 
-
-def get_nombre(self):
-    pass
-
-def get_apellido(self):
-    pass
-
-def set_nombre(self, dato_nombre):
-   pass
-
-def set_apellido(self, dato_apellido):
-   pass
-   
-   
-   
-   
-   
-   
     #se crean metodos normales de la clase
-def tomar_datos(self):
-    self.nombre_cliente = input("Ingrese su nombre: ")
-    self.apellido_cliente = input("Ingrese su apellido: ")
+    def tomar_datos(self):
+      self.nombre_cliente = input("Ingrese su nombre: ")
+      self.apellido_cliente = input("Ingrese su apellido: ")
     
-def procesar_datos (self):
-    print(f"El nombre del cliente es: {self.nombre_cliente}")
-    print(f"El apellido del cliente es: {self.apellido_cliente}")
+    def procesar_datos (self):
+      aux = self.nombre_cliente + " " + self.apellido_cliente
+
+    def mostrar_cliente(self):
+      print(f"Nombre: {self.nombre_cliente}")
+      print(f"Apellido: {self.apellido_cliente}")
+
+    def hacer_saludo(self, datoSaludo):
+       print(f"{datoSaludo} : {self.nombre_cliente}  {self.apellido_cliente}")
+    
+       
+
